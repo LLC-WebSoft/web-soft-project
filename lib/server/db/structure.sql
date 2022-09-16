9c601e14a84a56c9335d6c588b11dd0c78837863
DonVietnam <don.vietnam.js@gmail.com>
2022-09-16 17:38:50 +0300

    server: add github workflows and pre-commit hooks

--- lib/server/db/structure.sql
+++ lib/server/db/structure.sql
@@ -10,19 +10,29 @@ CREATE OR REPLACE FUNCTION CURRENT_SYSTEM_TIME() RETURNS timestamp without time 
 SELECT CURRENT_TIMESTAMP AT TIME ZONE ( SELECT "timezone" FROM "SystemSettings" );
 $$;
 
-CREATE TABLE "SystemUser"(
-  "username"  varchar( 128 ) NOT NULL,
-  "password" varchar( 255 ) NOT NULL,
-  "role" varchar( 32 ) NOT NULL,
-  "createdTime" timestamp(2) DEFAULT CURRENT_SYSTEM_TIME()
+CREATE TABLE "Role"(
+  "name" text NOT NULL
 );
+
+ALTER TABLE "Role" ADD CONSTRAINT "pkRole" PRIMARY KEY ( "name" );
+INSERT INTO "Role"(name) VALUES ('user');
+
+CREATE TABLE "SystemUser"(
+  "username"  char( 11 ) NOT NULL,
+  "password" text NOT NULL,
+  "role" text NOT NULL,
+  "createdTime" timestamp(3) DEFAULT CURRENT_SYSTEM_TIME()
+);
+
 ALTER TABLE "SystemUser" ADD CONSTRAINT "pkSystemUser" PRIMARY KEY ( "username" );
+ALTER TABLE "SystemUser" ADD CONSTRAINT "fkSystemUserRoleName" FOREIGN KEY ( "role" ) REFERENCES "Role"( "name" ) ON DELETE CASCADE;
 
 CREATE TABLE "Session"(
-  "username"  varchar( 128 ) NOT NULL,
-  "token" varchar( 64 ) NOT NULL,
-  "createdTime" timestamp(2) DEFAULT CURRENT_SYSTEM_TIME()
+  "username"  char( 11 ) NOT NULL,
+  "token" uuid NOT NULL,
+  "createdTime" timestamp(3) DEFAULT CURRENT_SYSTEM_TIME()
 );
+
 ALTER TABLE "Session" ADD CONSTRAINT "pkSession" PRIMARY KEY ( "token" );
 ALTER TABLE "Session" ADD CONSTRAINT "fkSessionUserUsername" FOREIGN KEY ( "username" ) REFERENCES  "SystemUser"( "username" ) ON DELETE CASCADE;
 
